d9561d9eafe832d61da709cf81cfd8ccf58b007c
Jad Jarkas <jadjarkas1@live.com>
2022-09-05 10:51:38 -0400

    added enemy class with getters and setters

--- champion.py
+++ champion.py
@@ -15,47 +15,47 @@ class champion:
         self.magic = cMagic
         self.coins = cCoin
 
-        def getChampionName(self):
-            return self.name
-        def getCurrentHealthStat(self):
-            return self.currentHealth
-        def getMaxHealth(self):
-            return self.maxHealth
-        def getXpStat(self):
-            return self.xp
-        def getMeleeAttackStat(self):
-            return self.meleeAttack
-        def getDefenseStat(self):
-            return self.defense
-        def getLuckStat(self):
-            return self.luck
-        def getRangedAttackStat(self):
-            return self.rangedAttack
-        def getMagicStat(self):
-            return self.magic
-        def getCoinAmount(self):
-            return self.coins
+    def getChampionName(self):
+        return self.name
+    def getCurrentHealthStat(self):
+        return self.currentHealth
+    def getMaxHealth(self):
+        return self.maxHealth
+    def getXpStat(self):
+        return self.xp
+    def getMeleeAttackStat(self):
+        return self.meleeAttack
+    def getDefenseStat(self):
+        return self.defense
+    def getLuckStat(self):
+        return self.luck
+    def getRangedAttackStat(self):
+        return self.rangedAttack
+    def getMagicStat(self):
+        return self.magic
+    def getCoinAmount(self):
+        return self.coins
         
-        def setChampionName(self, newName):
-            self.name = newName;
-        def setCurrentHealthStat(self, newCurrentHealth):
-            self.currentHealth = newCurrentHealth;
-        def setMaxHealth(self, newMaxHealth):
-            self.maxHealth = newMaxHealth;
-        def setXpStat(self, newXpStat):
-            self.xp = newXpStat
-        def setMeleeAttackStat(self, newMeleeAttack):
-            self.meleeAttack = newMeleeAttack
-        def setDefenseStat(self, newDefense):
-            self.defense = newDefense
-        def setLuckStat(self, newLuck):
-            self.luck = newLuck
-        def setRangedAttackStat(self, newRangedAttack):
-            self.rangedAttack = newRangedAttack
-        def setMagicStat(self, newMagic):
-            self.magic = newMagic
-        def setCoinAmount(self, newCoinTotal):
-            self.coins = newCoinTotal
+    def setChampionName(self, newName):
+        self.name = newName;
+    def setCurrentHealthStat(self, newCurrentHealth):
+        self.currentHealth = newCurrentHealth;
+    def setMaxHealth(self, newMaxHealth):
+        self.maxHealth = newMaxHealth;
+    def setXpStat(self, newXpStat):
+        self.xp = newXpStat
+    def setMeleeAttackStat(self, newMeleeAttack):
+        self.meleeAttack = newMeleeAttack
+    def setDefenseStat(self, newDefense):
+        self.defense = newDefense
+    def setLuckStat(self, newLuck):
+        self.luck = newLuck
+    def setRangedAttackStat(self, newRangedAttack):
+        self.rangedAttack = newRangedAttack
+    def setMagicStat(self, newMagic):
+        self.magic = newMagic
+    def setCoinAmount(self, newCoinTotal):
+        self.coins = newCoinTotal
 
 def createChampion():
     print("You slowly open your eyes and hazily look around\nMaybe picking a fight with a Giant wasn't the best idea")
@@ -105,3 +105,36 @@ print("\nYour Champion's Stats: ")
 character = champion(class_data[0], class_data[1], class_data[2], class_data[4], class_data[5], 100, 100, class_data[3], 0, 0)
 
 pprint(vars(character))
+
+class enemy:
+    def __init__ (self, eHealth, eAttack, ePower, eChance, eName):
+        self.health = eHealth
+        self.attack = eAttack
+        self.power = ePower
+        self.chance = eChance
+        self.name = eName
+
+    def getHealth(self):
+        return self.health
+    def getAttack(self):
+        return self.attack
+    def getPower(self):
+        return self.power
+    def getChance(self):
+        return self.chance
+    def getName(self):
+        return self.name
+    def setHealth(self, newHealth):
+        self.health = newHealth
+    def setAttack(self, newAttack):
+        self.attack = newAttack
+    def setPower(self, newPower):
+        self.power = newPower
+    def setChance(self, newChance):
+        self.chance = newChance
+    def setName(self, newName):
+        self.name = newName
+    
+    
+    
+    
